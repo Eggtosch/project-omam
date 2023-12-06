a957c6a50231df0b9e43097a412f121d7143150e
Oskar Munz <oskar.munz@gmx.de>
2023-12-06 21:25:33 +0100

    initial game setup
    Signed-off-by: Oskar Munz <oskar.munz@gmx.de>

---
+++ desktop/src/de/project/omam/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package de.project.omam;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import de.project.omam.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("project-omam");
+		new Lwjgl3Application(new Game(), config);
+	}
+}
